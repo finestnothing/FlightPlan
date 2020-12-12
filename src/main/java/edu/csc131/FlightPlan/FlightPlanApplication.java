a13aabe3b5d33cbb472c488523ee6c71ffe7566e
michaelingrum <mikeingrum1@gmail.com>
2020-12-11 22:28:52 -0800

    Removed flightplan results page and added new template courtesy of Mr. Michael Kinda. Added new js, css, and pics from the same template.
    Had to make some ajustments to folder names / paths.
    
    Removed unused imports from FlightPlanApplication.java

--- src/main/java/edu/csc131/FlightPlan/FlightPlanApplication.java
+++ src/main/java/edu/csc131/FlightPlan/FlightPlanApplication.java
@@ -1,10 +1,9 @@
 package edu.csc131.FlightPlan;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class FlightPlanApplication {
